a35b947df30219bf1c43f68cf9af10b0c7c6042e
harutiro <harutiro2027@outlook.jp>
2023-01-05 11:34:44 +0900

    testコードを作成した。

--- test_codeTests/test_codeTests.swift
+++ test_codeTests/test_codeTests.swift
@@ -10,27 +10,33 @@ import XCTest
 
 final class test_codeTests: XCTestCase {
 
-    override func setUpWithError() throws {
+    var viewController: ViewController!
+
+    override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        self.viewController = storyboard.instantiateInitialViewController() as? ViewController
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() throws {
+    func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        viewController.loadViewIfNeeded()
+        let subClass = viewController.subClass
+        let result = subClass?.multiply(num1: 7, num2: 28)
+        XCTAssertEqual(result, 196)
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
